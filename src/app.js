033120f98f4589336cc50d4cbdc3346697c4e546
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2025-03-12 08:29:04 -0400

    Route not found middleware

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-// const errorHandler = require('./middlewares/errorHandler');
+const notFound = require('./middlewares/not-found');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(notFound);
+
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
