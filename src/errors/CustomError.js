11c15bda9d468a59e3e3ff2f32fa5236874df13b
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2025-03-25 00:01:34 -0400

    StatusCodes Specific for consistency

--- src/errors/CustomError.js
+++ src/errors/CustomError.js
@@ -1,10 +1,12 @@
+const { StatusCodes } = require('http-status-codes');
+
 // Base error class for application-specific errors
 class CustomError extends Error {
   constructor(message) {
     super(message);
     this.name = this.constructor.name;
     // Set a default status code that can be overridden by child classes
-    this.statusCode = 500;
+    this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
   }
 }
 
