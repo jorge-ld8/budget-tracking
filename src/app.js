ac4948885e7a5ee872f35d570b910659201a13b4
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2025-03-26 14:09:29 -0400

    Security middlewares

--- src/app.js
+++ src/app.js
@@ -16,21 +16,36 @@ const { swaggerDocs } = require('./swagger');
 const path = require('path');
 const app = express();
 
+// Security packages
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
+   
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later'
+});
+
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(cors());
-// app.use(helmet());
+app.use(apiLimiter);
 app.use(helmet({
   contentSecurityPolicy: {
       directives: {
           upgradeInsecureRequests: null
       },
   },
-}))
+}));
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(hpp());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
 // Routes
