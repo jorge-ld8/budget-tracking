514b46263bdcc12120ea6eb567e50b99e7968dfd
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2025-05-04 19:12:06 -0400

    [BACK] Migrate config files from .js to .ts

--- src/config/s3Config.ts
+++ src/config/s3Config.ts
@@ -1,5 +1,6 @@
-import { S3Client } from '@aws-sdk/client-s3';
-import env from './env.js';
+import { S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
+import env from './env.ts';
+
 // Initialize S3 client
 const s3Client = new S3Client({
   region: env.AWS_REGION,
@@ -7,6 +8,6 @@ const s3Client = new S3Client({
     accessKeyId: env.AWS_ACCESS_KEY_ID,
     secretAccessKey: env.AWS_SECRET_ACCESS_KEY
   }
-});
+} as S3ClientConfig);
 
 export default s3Client;
